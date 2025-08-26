28ffb335813177e5ed3bb9eaa59ae7520c93a6dd
TEJASTATODE <tejastatode449@gmail.com>
2025-08-26 19:07:13 +0530

    Add school management backend APIs

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,7 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+const pool = mysql.createPool(process.env.DB_URL, {
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
