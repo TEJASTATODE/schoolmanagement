365aa5af05e41ddfedf081281093639d68f0f971
TEJASTATODE <tejastatode449@gmail.com>
2025-08-27 00:01:44 +0530

    Add school management backend APIs

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool(process.env.DB_URL, {
+    
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
